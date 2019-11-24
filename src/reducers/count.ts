2de989bcb27096ac927d0ea78bfa0cbbd9e3de10
kyruski <jamescboyett@gmail.com>
2019-11-23 22:29:44 -0800

    Changes the reducers to typescript

---
+++ src/reducers/count.ts
@@ -0,0 +1,11 @@
+import { Action } from "redux";
+export default (state: number = 0, action: Action): number => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
